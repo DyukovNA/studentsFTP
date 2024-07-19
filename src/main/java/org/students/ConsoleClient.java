c22267d5854fa0b0a68cfe9fa65a9a972fa53c05
DyukovNA <1ceptnpd@gmail.com>
2024-07-19 23:56:38 +0300

    Console menu done and cleanup

--- src/main/java/org/students/ConsoleClient.java
+++ src/main/java/org/students/ConsoleClient.java
@@ -4,35 +4,16 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class ConsoleClient {
-//    URLConnection connection;
     String link;
-    String JSONName = "students.json";
-    public ConsoleClient(String[] loginInfo){
+    String JSONName;
+    public ConsoleClient(String[] loginInfo, String filename){
         link = "ftp://" + loginInfo[0] + ":" + loginInfo[1] + "@" + loginInfo[2] + "/";
-        System.out.println(link);
-        try {
-            deleteStudent("1");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JSONName = filename;
     }
-    private URLConnection setConnection() {
-        try {
-            URL url = new URL(link);
-            URLConnection connection = url.openConnection();
-            connection.connect();
-//            System.out.println(getStudentInfo("2"));
-            return connection;
-        } catch (IOException e) {
-//            System.out.println("Unable to connect to server");
-            e.printStackTrace();
-        }
-        return null;
-    }
-    private URLConnection setConnectionFile(String filename) {
+
+    private URLConnection setConnection(String filename) {
         try {
             URL url = new URL(link + filename);
             URLConnection connection = url.openConnection();
@@ -44,30 +25,48 @@ public class ConsoleClient {
         }
         return null;
     }
-    public void menu(Integer action, String data) {
-        try {
-            URLConnection connection = setConnectionFile(JSONName);
-            switch (action) {
-                case 1:
-                    getListOfStudents();
-                    connection.getInputStream().close();
-                case 2:
-                    getStudentInfo(data);
-                    connection.getInputStream().close();
-                case 3:
-                    addStudent(data);
-                    connection.getInputStream().close();
-                case 4:
-                    deleteStudent(data);
-                    connection.getInputStream().close();
+    public void menu(Integer action, Scanner scanner) throws IOException {
+        switch (action) {
+            case 1: {
+                List<String> listOfStudents = getListOfStudents();
+                listOfStudents.forEach(System.out::println);
+                break;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            case 2: {
+                System.out.println("Please, student's ID:");
+                String id = scanner.next();
+                System.out.println(getStudentInfo(id));
+                break;
+            }
+            case 3: {
+                System.out.println("Please, enter student's name:");
+                String name = scanner.next();
+                if (addStudent(name)) {
+                    System.out.println("Student successfully added");
+                }
+                break;
+            }
+            case 4: {
+                System.out.println("Please, enter student's ID");
+                int idDelete = scanner.nextInt();
+                if (deleteStudent(Integer.toString(idDelete))) {
+                    System.out.println("Student successfully removed");
+                }
+                break;
+            }
+            case 5: {
+                System.out.println("Exiting session...");
+                break;
+            }
+            default: {
+                System.out.println("Unknown command");
+                break;
+            }
         }
-
     }
     public List<String> getListOfStudents() throws IOException {
-        URLConnection connection = setConnectionFile(JSONName);
+        URLConnection connection = setConnection(JSONName);
+        assert connection != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String line;
         List<String> students = new ArrayList<>();
@@ -83,15 +82,18 @@ public class ConsoleClient {
         return students;
     }
     public String getStudentInfo(String id) throws IOException {
-        URLConnection connection = setConnectionFile(JSONName);
+        URLConnection connection = setConnection(JSONName);
+        assert connection != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line = null;
+        String line;
         String toFind = "id" + id;
         while ((line = reader.readLine()) != null) {
             if (line.contains("id")) {
                 String s =  line.replaceAll("[\\s:\"{},]","");
                 if (s.equals(toFind)) {
-                    return reader.readLine().replaceAll("[\\s:\"{},]","").replace("name", "");
+                    return reader.readLine().replaceAll(
+                            "[\\s:\"{},]","").replace("name", ""
+                    );
                 }
             }
         }
@@ -99,8 +101,10 @@ public class ConsoleClient {
         return "No information found";
     }
     public boolean addStudent(String info) throws IOException {
-        URLConnection connectionUpload = setConnectionFile(JSONName);
-        URLConnection connection = setConnectionFile(JSONName);
+        URLConnection connectionUpload = setConnection(JSONName);
+        URLConnection connection = setConnection(JSONName);
+        assert connection != null;
+        assert connectionUpload != null;
 
         InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
         BufferedReader reader = new BufferedReader(inputStreamReader);
@@ -108,33 +112,34 @@ public class ConsoleClient {
         OutputStream outputStream = connectionUpload.getOutputStream();
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
 
-        String line = null;
-        String text = "";
-        String pattern = "\\s+\"id\": \\d+,";
-        Integer maxId = 0;
+        String line;
+        StringBuilder text = new StringBuilder();
+        int maxId = 0;
+
         while ((line = reader.readLine()) != null) {
-            if (Pattern.matches(pattern, line)) {
-                maxId = Integer.valueOf(
-                        line.replaceAll("[\\s:\"{},]","").replace("id", "")
+            if (line.matches("\\s+\"id\": \\d+,")) {
+                maxId = Integer.parseInt(
+                        line.replaceAll("[\\s:\"{},]", "").replace("id", "")
                 );
-                text += line + "\n";
+                text.append(line).append("\n");
             } else if (line.matches("\\s+}")) {
                 maxId += 1;
-                text += line + ",\n";
-                text += "    {\n      \"id\": " + maxId + ",\n      \"name\": "
-                        + '\"' + info + '\"' + "\n    }\n  ]\n}";
+                text.append(line).append(",\n");
+                text.append("    {\n      \"id\": ")
+                        .append(maxId).append(",\n      \"name\": \"")
+                        .append(info).append("\"\n    }\n  ]\n}");
                 break;
-            } else text += line + "\n";;
+            } else text.append(line).append("\n");
         }
 
-        writer.write(text);
+        writer.write(text.toString());
         writer.close();
         reader.close();
         return true;
     }
     public boolean deleteStudent(String id) throws IOException {
-        URLConnection connectionUpload = setConnectionFile(JSONName);
-        URLConnection connection = setConnectionFile(JSONName);
+        URLConnection connectionUpload = setConnection(JSONName);
+        URLConnection connection = setConnection(JSONName);
 
         assert connection != null;
         InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
@@ -178,7 +183,7 @@ public class ConsoleClient {
                         isRemoved = true;
                         break;
                     }
-                    line = reader.readLine();
+                    reader.readLine();
                     isRemoved = true;
                 } else {
                     studentBlock.append(line).append("\n");
--- src/main/java/org/students/Main.java
+++ src/main/java/org/students/Main.java
@@ -2,37 +2,31 @@ package org.students;
 
 import java.io.Console;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-/*        Console console = System.console();
-        Scanner scanner = new Scanner(console.reader());
-        console.writer().println("Please, enter your username, password and the server's IP-address");
-        String[] loginInfo = scanner.nextLine().split(" ");
-        ConsoleClient consoleClient = new ConsoleClient(loginInfo);
-        console.writer().println("Connection successful");
-        console.writer().println("1.\tGet list of students by name\n" +
-                "2.\tGet student info by id \n" +
-                "3.\tAdd student to list\n" +
-                "4.\tDelete student by id\n" +
-                "5.\tExit\n"
-        );
-        Integer action = scanner.nextInt();
-        if (action < 5 && scanner.hasNext()) {
-            consoleClient.menu(action, scanner.next());
-        } else if (action == 5) {
-            console.writer().println("Exiting program");
-        } else {
-            console.writer().println("Unexpected input");
-
-        }*/
         try {
-            String[] loginInfo = "TestUser 1234 0.0.0.0".split(" ");
-            ConsoleClient consoleClient = new ConsoleClient(loginInfo);
-
-        } catch (Exception e) {
+            String JSONName = "students.json"; // Имя файла на сервере
+            Console console = System.console();
+            Scanner scanner = new Scanner(console.reader());
+            console.writer().println("Please, enter your username, password and the server's IP-address:");
+            String[] loginInfo = scanner.nextLine().split(" ");
+            ConsoleClient consoleClient = new ConsoleClient(loginInfo, JSONName);
+            console.writer().println("Connection successful");
+            console.writer().println("1.\tGet list of students by name\n" +
+                    "2.\tGet student info by id \n" +
+                    "3.\tAdd student to list\n" +
+                    "4.\tDelete student by id\n" +
+                    "5.\tExit\n"
+            );
+            int action;
+            do {
+                System.out.println("Select an action:");
+                action = scanner.nextInt();
+                consoleClient.menu(action, scanner);
+            } while (action != 5);
+        }  catch (IOException e) {
             e.printStackTrace();
         }
     }
